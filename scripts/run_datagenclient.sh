367be0b07687f9a72b91bd7aa6a9f6c448572e39
John Gates <jgates@slac.stanford.edu>
2020-10-01 11:09:35 -0700

    Added partitioner to docker build.

--- scripts/run_datagenclient.sh
+++ scripts/run_datagenclient.sh
@@ -3,5 +3,7 @@ source /qserv/stack/loadLSST.bash
 setup -t qserv-dev qserv_distrib
 cd /home/qserv/sphgeom
 setup -k -r . -t qserv-dev
+cd /home/qserv/partition
+setup -k -r . -t qserv-dev
 cd /home/qserv/dax_data_generator/
 python bin/datagenclient.py "$@"
