7bdd624f183efd434fb92a562d824a9e3f178098
John Gates <jgates@slac.stanford.edu>
2020-10-22 12:53:43 -0700

    Added visit table generation to server start script.

--- scripts/run_datagenserver.sh
+++ scripts/run_datagenserver.sh
@@ -2,4 +2,6 @@
 source /qserv/stack/loadLSST.bash
 setup -t qserv-dev qserv_distrib
 cd /home/qserv/dax_data_generator/
+# Folowing needs to be generalized
+python bin/makevisits.py -o configs/fakedb/pregenerated/visit_table.csv
 python bin/datagenserver.py "$@"
